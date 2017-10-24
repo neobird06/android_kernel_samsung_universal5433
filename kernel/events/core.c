f5bb3b5853bfbefae3e3accb5ffd6fe88d13c293
Yonghong Song <yhs@fb.com>
2017-10-23 23:53:07 -0700

    UPSTREAM: bpf: use the same condition in perf event set/free bpf handler
    This is a cleanup such that doing the same check in
    perf_event_free_bpf_prog as we already do in
    perf_event_set_bpf_prog step.
    
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 0b4c6841fee03e096b735074a0c4aab3a8e92986)
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 121213201
    Bug: 138317270
    Test: build & boot cuttlefish
    Change-Id: Ie423e73a73be29e8ef50cc22dbb03e14e241c8de
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    (cherry picked from commit 6830dfdf065702a1336e838418f9e59939cad585)

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5956,6 +5956,17 @@ static void perf_event_free_bpf_prog(struct perf_event *event)
 	if (event->attr.type != PERF_TYPE_TRACEPOINT) {
 		perf_event_free_bpf_handler(event);
 		return;
+	struct bpf_prog *prog;
+
+	if (event->attr.type != PERF_TYPE_TRACEPOINT) {
+		perf_event_free_bpf_handler(event);
+		return;
+	}
+
+	prog = event->tp_event->prog;
+	if (prog) {
+		event->tp_event->prog = NULL;
+		bpf_prog_put(prog);
 	}
 	perf_event_detach_bpf_prog(event);
 }
