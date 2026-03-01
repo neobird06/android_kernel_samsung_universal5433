c4f1a3b62674c62cd06abb9c88330512996d1e74
neobird06 <skywizard0413@gmail.com>
2026-03-01 20:19:14 +0800

    cgroup: Add compat cgroup2 fs Change-Id: Ia5e56b6706c8445f8267444d3544c9e9af48468b

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -91,6 +91,8 @@ static DEFINE_MUTEX(cgroup_mutex);
 
 static DEFINE_MUTEX(cgroup_root_mutex);
 
+static struct file_system_type compat_cgroup2_fs_type;
+
 /*
  * cgroup destruction makes heavy use of work items and there can be a lot
  * of concurrent destructions.  Use a separate workqueue so that cgroup
@@ -1585,10 +1587,19 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 #ifdef CONFIG_TIMA_RKP_RO_CRED
 	struct cred *root_cred = &init_cred;
 #endif  /* CONFIG_TIMA_RKP_RO_CRED */
+	bool is_v2 = fs_type == &compat_cgroup2_fs_type;
+
 	/* First find the desired set of subsystems */
-	mutex_lock(&cgroup_mutex);
-	ret = parse_cgroupfs_options(data, &opts);
-	mutex_unlock(&cgroup_mutex);
+	if(is_v2){
+	       memset(&opts, 0, sizeof(opts));
+	       opts.none = true;
+	}
+
+	else{
+	       mutex_lock(&cgroup_mutex);
+	       ret = parse_cgroupfs_options(data, &opts);
+	       mutex_unlock(&cgroup_mutex);
+	}
 	if (ret)
 		goto out_err;
 
@@ -1782,6 +1793,12 @@ static void cgroup_kill_sb(struct super_block *sb) {
 
 static struct file_system_type cgroup_fs_type = {
 	.name = "cgroup",
+	.mount = cgroup_mount,
+	.kill_sb = cgroup_kill_sb,
+};
+
+static struct file_system_type compat_cgroup2_fs_type = {
+	.name = "cgroup2",
 	.mount = cgroup_mount,
 	.kill_sb = cgroup_kill_sb,
 };
@@ -4721,6 +4738,12 @@ int __init cgroup_init(void)
 	}
 
 	err = register_filesystem(&cgroup_fs_type);
+	if (err < 0) {
+		kobject_put(cgroup_kobj);
+		goto out;
+	}
+
+	err = register_filesystem(&compat_cgroup2_fs_type);
 	if (err < 0) {
 		kobject_put(cgroup_kobj);
 		goto out;
